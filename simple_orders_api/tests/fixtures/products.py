1901e0f7d36c4f735d75fba5889a04e88ffe485e
Shubham Singh <32100935+ShubhamSinghCMR@users.noreply.github.com>
2025-05-06 11:42:12 +0530

    Removed Project

--- simple_orders_api/tests/fixtures/products.py
+++
@@ -1,8 +0,0 @@
-from app.models import Product
-
-
-def create_sample_products(db):
-    products = [Product(name=f"Product {i}", price=10.0 * i) for i in range(1, 6)]
-    db.add_all(products)
-    db.commit()
-    return products
